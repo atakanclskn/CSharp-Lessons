e4e7fc0526c0afcc9edae03ee74876a7e4dfd710
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-15 22:31:01 +0300

    Yeni "FloatVeriTipi" projesi eklendi
    Çeşitli dosyalarda yapılan değişiklikler, yeni bir "FloatVeriTipi" projesinin eklenmesini içermektedir.
    - Çözüm dosyasında "FloatVeriTipi" projesi tanımlandı ve yapılandırma ayarları eklendi.
    - "App.config" dosyası, uygulamanın çalışması için gerekli temel yapılandırma bilgileri ile oluşturuldu.
    - "FloatVeriTipi.csproj" dosyası, proje ayarlarını ve bağımlılıklarını tanımlayan bir MSBuild projesi olarak eklendi.
    - "Program.cs" dosyasında, float veri tipini kullanan basit bir program yazıldı.
    - "AssemblyInfo.cs" dosyasında, projenin genel bilgileri ve COM görünürlüğü ile ilgili öznitelikler eklendi.

---
+++ FloatVeriTipi/Program.cs
@@ -0,0 +1,39 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// FloatVeriTipi adında bir ad alanı tanımlar
+namespace FloatVeriTipi
+{
+    // Program sınıfı
+    internal class Program
+    {
+        // Programın başlangıç noktası
+        static void Main(string[] args)
+        {
+            // float ondalıklı sayılar için kullanılır, 4 byte yer kaplar
+
+            float ondalikliSayi = 35.56f; // ondalikliSayi'ya değer atanır
+
+            float ondalikliSayi2 = 12.5f; // ondalikliSayi2'ye değer atanır
+
+            Console.WriteLine(ondalikliSayi); // ilk sayıyı ekrana yazdırır
+            Console.WriteLine(ondalikliSayi2); // ikinci sayıyı ekrana yazdırır
+
+            float max = float.MaxValue; // float tipinin alabileceği en büyük değer
+            Console.WriteLine(max); // max değeri ekrana yazdırır
+
+            float min = float.MinValue; // float tipinin alabileceği en küçük değer
+            Console.WriteLine(min); // min değeri ekrana yazdırır
+
+            Console.ReadLine(); // Programın hemen kapanmaması için bekler
+        }
+    }
+}
