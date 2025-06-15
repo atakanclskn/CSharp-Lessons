4f0d4d30ec312959213c79f0e265bd62f537d5b6
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-16 00:35:46 +0300

    Yeni "CarpmaVeBolmeOperatorleri" projesi eklendi
    Çeşitli dosyalarda yapılan değişikliklerle birlikte, çarpma ve bölme işlemlerini gerçekleştiren "CarpmaVeBolmeOperatorleri" adlı yeni bir proje oluşturuldu.
    `CSharp-Lessons.sln` dosyasına yeni proje bilgileri eklendi ve yapılandırma ayarları güncellendi.
    `App.config` dosyası, uygulamanın çalışması için gerekli temel yapılandırma bilgilerini içerir.
    `CarpmaVeBolmeOperatorleri.csproj` dosyası, proje ayarlarını ve bağımlılıklarını tanımlamak için oluşturuldu.
    `Program.cs` dosyasında ana program sınıfı tanımlandı.
    `AssemblyInfo.cs` dosyasında projenin genel bilgileri ve COM görünürlüğü ile ilgili ayarlar güncellendi.

---
+++ CarpmaVeBolmeOperatorleri/Program.cs
@@ -0,0 +1,36 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// CarpmaVeBolmeOperatorleri adında bir ad alanı tanımlar
+namespace CarpmaVeBolmeOperatorleri
+{
+    // Program sınıfı
+    internal class Program
+    {
+        // Programın başlangıç noktası
+        static void Main(string[] args)
+        {
+            // Çarpma (*) ve bölme (/) operatörleri: Sayısal değerler üzerinde çarpma ve bölme işlemleri yapmak için kullanılır
+
+            int sayi1 = 4; // Birinci sayı
+            int sayi2 = 8; // İkinci sayı
+            int sayi3 = 5; // Üçüncü sayı
+            int sayi4 = 5; // Dördüncü sayı
+
+            int sonuc1 = (sayi1 * sayi2); // Çarpma işlemi: 4 * 8 = 32
+            int sonuc2 = (sayi3 / sayi4); // Bölme işlemi: 5 / 5 = 1 (tam sayı bölmesi)
+
+            Console.WriteLine(sonuc1); // Çarpma sonucunu ekrana yazdırır
+            Console.WriteLine(sonuc2); // Bölme sonucunu ekrana yazdırır
+            Console.ReadLine(); // Programın hemen kapanmaması için bekler
+        }
+    }
+}
