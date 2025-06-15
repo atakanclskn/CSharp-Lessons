e5c568be48861af3e69e9359a12bdc5ea2e49733
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-16 00:25:24 +0300

    Yeni proje: ToplamaVeCikarmaOperatorleri eklendi
    Çözüm dosyasına "ToplamaVeCikarmaOperatorleri" projesi eklendi.
    Uygulama, toplama ve çıkarma işlemlerini gerçekleştirmektedir.
    "App.config" dosyası, gerekli yapılandırma bilgileri ile güncellendi.
    "Program.cs" dosyasında toplama ve çıkarma işlemlerini yapan bir program eklendi.
    Yeni proje için "ToplamaVeCikarmaOperatorleri.csproj" dosyası oluşturuldu ve yapılandırma ayarları tanımlandı.
    "AssemblyInfo.cs" dosyasına projenin genel bilgilerini içeren öznitelikler eklendi.

---
+++ ToplamaVeCikarmaOperatorleri/Program.cs
@@ -0,0 +1,36 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// ToplamaVeCikarmaOperatorleri adında bir ad alanı tanımlar
+namespace ToplamaVeCikarmaOperatorleri
+{
+    // Program sınıfı
+    internal class Program
+    {
+        // Programın başlangıç noktası
+        static void Main(string[] args)
+        {
+            // Toplama (+) ve çıkarma (-) operatörleri: Sayısal değerler üzerinde toplama ve çıkarma işlemleri yapmak için kullanılır
+
+            int sayi1 = 5; // Birinci sayı
+            int sayi2 = 12; // İkinci sayı
+            int sayi3 = 13; // Üçüncü sayı
+
+            int sonuc = sayi1 + sayi2; // Toplama işlemi: 5 + 12 = 17
+
+            int sonuc2 = sayi1 - sayi3; // Çıkarma işlemi: 5 - 13 = -8
+
+            Console.WriteLine(sonuc); // Toplama sonucunu ekrana yazdırır
+            Console.WriteLine(sonuc2); // Çıkarma sonucunu ekrana yazdırır
+            Console.ReadLine(); // Programın hemen kapanmaması için bekler
+        }
+    }
+}
