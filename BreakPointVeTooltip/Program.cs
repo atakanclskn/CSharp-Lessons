4f981a680a4b4b3174e35811c1f67cfc44aa02a6
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-21 02:42:22 +0300

    Yeni "BreakPointVeTooltip" projesi eklendi
    Çözüm dosyasına ("CSharp-Lessons.sln") "BreakPointVeTooltip" projesi eklendi ve yapılandırma ayarları güncellendi.
    "App.config" dosyasına çalışma zamanı bilgileri eklendi.
    "BreakPointVeTooltip.csproj" dosyasına proje ayarları ve bağımlılıklar tanımlandı.
    "Program.cs" dosyasına basit bir çarpma işlemi gerçekleştiren program sınıfı eklendi.
    "AssemblyInfo.cs" dosyasına projenin genel bilgilerini tanımlayan öznitelikler eklendi.

---
+++ BreakPointVeTooltip/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// BreakpointVeTooltip adında bir ad alanı tanımlar
+namespace BreakPointVeTooltip
+{
+    // Program sınıfı
+    internal class Program
+    {
+        // Programın başlangıç noktası
+        static void Main(string[] args)
+        {
+            int sayi = 10; // Bir değişken tanımlanır
+            int sonuc = sayi * 2; // İşlem yapılır
+
+            Console.WriteLine(sonuc); // Sonuç ekrana yazdırılır
+            Console.ReadLine(); // Programın hemen kapanmaması için bekler
+        }
+    }
+}
