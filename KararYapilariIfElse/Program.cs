db4e4b1c3771c599db0909d32e2cb93d077459d1
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-25 19:48:02 +0300

    Yeni "KararYapilariIfElse" projesi eklendi
    "KararYapilariIfElse" adında yeni bir C# projesi oluşturuldu.
    Bu proje, if-else karar yapısını kullanarak basit bir not kontrolü gerçekleştirmektedir.
    App.config, KararYapilariIfElse.csproj, Program.cs ve AssemblyInfo.cs dosyalarında gerekli yapılandırmalar ve kodlar eklendi.
    Program.cs dosyasında, not değerine göre başarılı veya başarısız mesajı gösteren ana mantık geliştirildi.
    AssemblyInfo.cs dosyasında projenin genel bilgileri ve COM görünürlüğü ayarları güncellendi.

---
+++ KararYapilariIfElse/Program.cs
@@ -0,0 +1,39 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// KararYapilariIfElse adında bir ad alanı tanımlar
+namespace KararYapilariIfElse
+{
+    // Program sınıfı
+    internal class Program
+    {
+        // Programın başlangıç noktası
+        static void Main(string[] args)
+        {
+            // if-else: Karar yapısıdır. Belirli bir koşulun doğru (true) olup olmadığına göre kodun farklı bölümlerinin çalışmasını sağlar.
+            // if (koşul) { ... } : Koşul doğruysa buradaki kodlar çalışır.
+            // else { ... } : Koşul yanlışsa buradaki kodlar çalışır.
+
+            int not = 75; // not değişkenine 75 değeri atanır
+
+            if (not > 50) // Eğer not 50'den büyükse
+            {
+                Console.WriteLine("Ogrenci Basarılı!"); // Bu satır çalışır
+                Console.ReadLine();
+            }
+            else // Eğer not 50'den büyük değilse
+            {
+                Console.WriteLine("Ogrenci basarili degil!"); // Bu satır çalışır
+                Console.ReadLine();
+            }
+        }
+    }
+}
