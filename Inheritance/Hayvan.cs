6fdd0a0332124399c77368fe58cbeca78826016a
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-26 02:45:32 +0300

    Hayvan ve Kedi sınıfları eklendi
    `Inheritance.csproj` dosyasına `Hayvan.cs` ve `Kedi.cs` dosyaları eklendi. `Hayvan` sınıfı, hayvanların adını ve yaşını tutan özellikler ile bir ses çıkarma metodunu içeriyor. `Kedi` sınıfı, `Hayvan` sınıfından kalıtım alarak tüm özellikleri ve metotları miras alıyor.

---
+++ Inheritance/Hayvan.cs
@@ -0,0 +1,29 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// Inheritance adında bir ad alanı tanımlar
+namespace Inheritance
+{
+    // Hayvan adında bir temel (base) sınıf tanımlanır
+    public class Hayvan
+    {
+        // Hayvanın adı
+        public string isim { get; set; }
+        // Hayvanın yaşı
+        public int yas { get; set; }
+
+        // Hayvanın ses çıkarma metodu
+        public void SesCikar()
+        {
+            Console.WriteLine("Hayvan ses çıkarıyor.");
+        }
+    }
+}
---
+++ Inheritance/Kedi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Inheritance adında bir ad alanı tanımlar
+namespace Inheritance
+{
+    // Kedi sınıfı, Hayvan sınıfından kalıtım (inheritance) alır
+    public class Kedi : Hayvan
+    {
+        // Kedi sınıfı boş, ama Hayvan'dan tüm özellikleri ve metotları miras alır
+    }
+}
