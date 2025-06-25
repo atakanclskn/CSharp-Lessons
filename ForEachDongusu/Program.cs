75629a86cda9ec36dd71d80dc25852e4c50948ad
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-25 23:04:52 +0300

    Yeni "ForEachDongusu" projesi eklendi
    C# dilinde yazılmış "ForEachDongusu" adlı yeni bir proje oluşturuldu.
    Çözüm dosyasına gerekli yapılandırmalar eklendi.
    "App.config" dosyası uygulama ayarlarını içeriyor.
    Proje ayarları ve bağımlılıkları "ForEachDongusu.csproj" dosyasında tanımlandı.
    "Program.cs" dosyasında bir dizi üzerinde döngü ile elemanları yazdıran örnek kod eklendi.
    "AssemblyInfo.cs" dosyasında projenin genel bilgileri güncellendi.

---
+++ ForEachDongusu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachDongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach dongusu...
+
+            string[] isimler = { "Enes", "Aysun", "Sevcan", "Elif", "Eren" };
+            foreach (string i in isimler)
+            {
+                if (i == "Enes")
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+        Console.ReadLine();
+        }
+    }
+}
