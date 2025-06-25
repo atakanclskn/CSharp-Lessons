e8ecaca3997772420642963bfc9b6cca6b4b1cff
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-26 02:00:05 +0300

    Yeni "Property" projesi eklendi ve dosyalar oluşturuldu.
    Çeşitli dosyalarda yapılan değişiklikler, yeni bir "Property" projesinin eklenmesini ve bu projeye ait dosyaların oluşturulmasını içermektedir. `CSharp-Lessons.sln` çözüm dosyasına "Property" projesi eklenmiş ve yapılandırma ayarları güncellenmiştir. `App.config` dosyası, uygulamanın çalışması için gerekli olan yapılandırma ayarlarını içeren bir XML yapısı ile oluşturulmuştur. `Musteri.cs` ve `Program.cs` dosyalarına, "Musteri" sınıfı ve bu sınıfın kullanımını gösteren bir program eklenmiştir. `Property.csproj` dosyası, projenin yapılandırma ayarlarını ve bağımlılıklarını tanımlayan bir MSBuild projesi olarak oluşturulmuştur. `AssemblyInfo.cs` dosyası, projenin genel bilgilerini içeren öznitelikler ile güncellenmiştir.

---
+++ Property/Musteri.cs
@@ -0,0 +1,40 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// Property adında bir ad alanı tanımlar
+namespace Property
+{
+    // Musteri sınıfı
+    public class Musteri
+    {
+        // Alan (field) genellikle private olarak tanımlanır
+        private int kilo;
+
+        // Property (özellik): Alanlara kontrollü erişim sağlar
+        // get: Değeri okumak için kullanılır
+        // set: Değeri değiştirmek için kullanılır
+        public int KILO
+        {
+            get
+            {
+                return kilo; // kilo değerini döndürür
+            }
+            set
+            {
+                // Değer negatifse 0 olarak ayarlanır, değilse gelen değer atanır
+                if (value < 0)
+                    kilo = 0;
+                else
+                    kilo = value;
+            }
+        }
+    }
+}
---
+++ Property/Program.cs
@@ -0,0 +1,29 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// Property adında bir ad alanı tanımlar
+namespace Property
+{
+    // Program sınıfı
+    internal class Program
+    {
+        // Programın başlangıç noktası
+        static void Main(string[] args)
+        {
+            // Musteri sınıfından bir nesne oluşturulur
+            Musteri musteri1 = new Musteri();
+
+            // Property (özellik) üzerinden değer ataması yapılır
+            musteri1.KILO = 88; // set bloğu çalışır
+            Console.WriteLine(musteri1.KILO); // get bloğu çalışır ve ekrana yazdırılır
+        }
+    }
+}
