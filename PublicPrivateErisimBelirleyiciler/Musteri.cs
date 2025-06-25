0d31f8045be52d2eb3b4da0b7251d3ef91aeffe1
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-26 00:48:00 +0300

    Yeni proje "PublicPrivateErisimBelirleyiciler" eklendi
    CSharp-Lessons.sln dosyasına yeni proje eklendi ve yapılandırma ayarları güncellendi.
    App.config dosyası, uygulama için gerekli temel yapılandırma bilgilerini içeriyor.
    Musteri.cs dosyasında, public ve private erişim belirleyicilerini örnekleyen bir Musteri sınıfı tanımlandı.
    Program.cs dosyasında, Musteri sınıfının kullanıldığı bir ana program yapısı oluşturuldu.
    PublicPrivateErisimBelirleyiciler.csproj dosyası yeni projenin yapılandırma ayarlarını içeriyor.
    AssemblyInfo.cs dosyasında, projenin genel bilgileri ve COM görünürlüğü ile ilgili ayarlar güncellendi.

---
+++ PublicPrivateErisimBelirleyiciler/Musteri.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicPrivateErisimBelirleyiciler
+{
+    internal class Musteri
+    {
+        public string isim;
+        public string surname;
+        public int maas;
+        public string sex;
+        private string yas;
+
+        public Musteri()
+        {
+            Console.WriteLine("Musteri sinifinin constructor yapisi calisti!");
+        }
+
+        public void musterininBilgileriniGoster()
+        {
+            Console.WriteLine("Isim Soyisim"+surname+isim);
+            Console.WriteLine("Maas:"+maas);
+            Console.WriteLine("Cinsiyet:"+sex);
+            Console.WriteLine("Yas degeri:"+yas);
+        }
+
+        private void isimSoyisimYazdir(string musteriisim,string musteriSoyisim)
+        {
+            Console.WriteLine("Musterinin Isim Soyismi:"+musteriisim+" "+musteriSoyisim);
+        }
+    }
+}
+
+    
---
+++ PublicPrivateErisimBelirleyiciler/Program.cs
@@ -0,0 +1,34 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// PublicPrivateErisimBelirleyiciler adında bir ad alanı tanımlar
+namespace PublicPrivateErisimBelirleyiciler
+{
+    // Program sınıfı
+    internal class Program
+    {
+        // Programın başlangıç noktası
+        static void Main(string[] args)
+        {
+            // public ve private erişim belirleyicileri nedir?
+            // public: Sınıfın dışından erişilebilen alan, metot veya özelliklerdir.
+            // private: Sadece tanımlandığı sınıf içinde erişilebilen alan, metot veya özelliklerdir.
+
+            Musteri musteri1 = new Musteri(); // Musteri sınıfından bir nesne oluşturulur
+
+            musteri1.isim = "Atakan";      // public alan: dışarıdan erişilebilir
+            musteri1.surname = "Caliskan"; // public alan: dışarıdan erişilebilir
+            musteri1.maas = 6636;          // public alan: dışarıdan erişilebilir
+            musteri1.sex = "E";            // public alan: dışarıdan erişilebilir
+            // musteri1.yas;               // private alan: dışarıdan erişilemez (hata verir)
+        }
+    }
+}
