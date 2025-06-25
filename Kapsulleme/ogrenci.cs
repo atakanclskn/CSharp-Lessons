59ebbcc774d81fc1324a2adb2ac283f7563d417e
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-26 01:43:39 +0300

    Yeni ogrenci sınıfı eklendi ve projeye dahil edildi.
    Kapsulleme.csproj dosyasına "ogrenci.cs" adlı yeni bir kaynak dosyası eklendi.
    ogrenci.cs dosyasında "Kapsulleme" adında bir ad alanı oluşturuldu ve bu ad alanı içinde "ogrenci" adında bir iç sınıf tanımlandı.
    Bu sınıf, öğrencinin ismi, yaşı ve notu gibi özellikleri kapsülleyen üç özel alan içeriyor.
    Ayrıca, bu alanlara erişim sağlayan özellikler (Isim, Yas, Not) tanımlandı.
    Her bir özellik, ilgili alanın değerini almak ve ayarlamak için get ve set erişimcilerini kullanıyor.

---
+++ Kapsulleme/ogrenci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsulleme
+{
+    internal class ogrenci
+    {
+        private string isim;   // Öğrencinin ismi
+        private int yas;       // Öğrencinin yaşı
+        private double not;    // Öğrencinin notu
+
+        public string Isim
+        {
+            get { return isim; }      // ismi döndürür
+            set { isim = value; }     // ismi değiştirir
+        }
+
+        public int Yas
+        {
+            get { return yas; }       // yaşı döndürür
+            set { yas = value; }      // yaşı değiştirir
+        }
+
+        public double Not
+        {
+            get { return not; }       // notu döndürür
+            set { not = value; }      // notu değiştirir
+        }
+    }
+}
