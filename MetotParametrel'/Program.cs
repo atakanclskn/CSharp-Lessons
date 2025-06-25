4ef59a8bfb301b69481e1d27f626814e313ab99c
Atakan Çalışkan <atakanclskn@outlook.com>
2025-06-25 23:41:20 +0300

    Yeni proje: MetotParametreli eklendi
    Çözüm dosyasına ("CSharp-Lessons.sln") "MetotParametreli" adlı yeni bir proje eklendi ve bu projeye ait yapılandırma bilgileri oluşturuldu.
    
    "App.config" dosyasına, uygulamanın desteklediği .NET sürümü eklendi.
    
    "MetotParametreli.csproj" dosyası, proje ayarlarını ve bağımlılıklarını tanımlayan XML içeriği ile oluşturuldu.
    
    "Program.cs" dosyasına, kullanıcıdan iki sayı alıp toplamını hesaplayan bir program eklendi.
    
    "AssemblyInfo.cs" dosyasında, projenin genel bilgilerini tanımlayan öznitelikler güncellendi.

---
+++ MetotParametrel'/Program.cs
@@ -0,0 +1,43 @@
+﻿// Gerekli sistem kütüphanelerini ekler
+using System;
+// Koleksiyonlar için kütüphane
+using System.Collections.Generic;
+// LINQ işlemleri için kütüphane
+using System.Linq;
+// Metin işlemleri için kütüphane
+using System.Text;
+// Çoklu görevler için kütüphane
+using System.Threading.Tasks;
+
+// MetotParametrel_ adında bir ad alanı tanımlar
+namespace MetotParametreli
+{
+    // Program sınıfı
+    internal class Program
+    {
+        // Programın başlangıç noktası
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan iki sayı alınır
+            Console.WriteLine("Lutfen 1. sayiyi giriniz: ");
+            int a = Convert.ToInt32(Console.ReadLine()); // 1. sayı alınır
+            Console.WriteLine("Lutfen 2. sayiyi giriniz: ");
+            int b = Convert.ToInt32(Console.ReadLine()); // 2. sayı alınır
+
+            // Parametreli metot çağrılır ve a, b değerleri gönderilir
+            toplamaYap(a, b);
+        }
+
+        // Parametreli metot nedir?
+        // Parametreli metotlar, dışarıdan değer alabilen ve bu değerlerle işlem yapabilen metotlardır.
+        // Metot tanımında parantez içinde belirtilen değişkenlere "parametre" denir.
+        // Çağrılırken bu parametrelere uygun değerler (argümanlar) gönderilir.
+
+        static void toplamaYap(int sayi1, int sayi2)
+        {
+            // Gönderilen iki sayının toplamını ekrana yazdırır
+            Console.WriteLine("Gonderilen sayilarin toplami = " + (sayi1 + sayi2));
+            Console.ReadLine(); // Programın hemen kapanmaması için bekler
+        }
+    }
+}
